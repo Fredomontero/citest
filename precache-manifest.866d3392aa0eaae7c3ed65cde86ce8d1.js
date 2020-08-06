c3bb25d8a0d4394a0fd7442c7cc2833fde8773c2
Christian Montero Martínez <christianmontero08@gmail.com>
2020-08-05 17:29:03 -0700

    Updates

---
+++ precache-manifest.866d3392aa0eaae7c3ed65cde86ce8d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60f983e0d0acc95fca72cd699450c21",
+    "url": "/citest/index.html"
+  },
+  {
+    "revision": "950b7dbb0f9254a47aa2",
+    "url": "/citest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c36577a4111fbe8777cc",
+    "url": "/citest/static/js/2.eb88750a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/citest/static/js/2.eb88750a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "950b7dbb0f9254a47aa2",
+    "url": "/citest/static/js/main.bc92d644.chunk.js"
+  },
+  {
+    "revision": "61c3cfa05c0c9b1eb035",
+    "url": "/citest/static/js/runtime-main.47861644.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/citest/static/media/logo.5d5d9eef.svg"
+  }
+]);
